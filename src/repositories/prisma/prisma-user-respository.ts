c0a50ffb5959a83b7e503c674cb8fa70c5057dda
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-10-16 10:33:34 -0300

    feat: repository do user

---
+++ src/repositories/prisma/prisma-user-respository.ts
@@ -0,0 +1,36 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../../database/prisma";
+import { UserRepository } from "../user-repository";
+
+export class PrismaUserRepository implements UserRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    return user;
+  }
+
+  async findMany() {
+    const users = await prisma.user.findMany();
+    return users;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    });
+    return user;
+  }
+}
---
+++ src/repositories/user-repository.ts
@@ -0,0 +1,8 @@
+import { Prisma, User } from "@prisma/client";
+
+export interface UserRepository {
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findMany(): Promise<User[] | null>;
+  create(data: Prisma.UserCreateInput): Promise<User>;
+}
