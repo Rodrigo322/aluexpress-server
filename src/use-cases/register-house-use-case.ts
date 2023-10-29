6d45d4fd807775438e08dc31294a84294b51b4f7
Rodrigo322 <rodrigollrdglucas@gmail.com>
2023-10-29 13:53:20 -0300

    feat: caso de uso para criar uma nova casa vinculado a um usuário

---
+++ src/use-cases/register-house-use-case.ts
@@ -0,0 +1,70 @@
+import { House } from "@prisma/client";
+
+import { IHouseRepository } from "../repositories/house-repository";
+import { UserRepository } from "../repositories/user-repository";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+
+interface RegisterHouseUseCaseRequest {
+  userId: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  price: string;
+  description: string;
+  footage: number;
+  status: boolean;
+  contract: boolean;
+  contract_period?: number;
+  water_and_electricity_included: boolean;
+}
+
+interface RegisterHouseUseCaseResponse {
+  house: House;
+}
+
+export class RegisterHouseUseCase {
+  constructor(
+    private houseRepository: IHouseRepository,
+    private userRepository: UserRepository
+  ) {}
+
+  async execute({
+    userId,
+    address,
+    contract,
+    description,
+    footage,
+    latitude,
+    longitude,
+    price,
+    status,
+    water_and_electricity_included,
+    contract_period,
+  }: RegisterHouseUseCaseRequest): Promise<RegisterHouseUseCaseResponse> {
+    const user = await this.userRepository.findById(userId);
+
+    if (user) {
+      throw new ResourceNotFoundError();
+    }
+
+    const house = await this.houseRepository.create({
+      User: {
+        connect: {
+          id: userId,
+        },
+      },
+      address,
+      contract,
+      description,
+      footage,
+      latitude,
+      longitude,
+      price,
+      status,
+      water_and_electricity_included,
+      contract_period,
+    });
+
+    return { house };
+  }
+}
